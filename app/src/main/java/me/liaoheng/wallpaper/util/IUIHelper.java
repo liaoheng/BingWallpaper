16af455799d42ab66dc19c2a176a6c354472ef76
liaoheng <liaohengcn@gmail.com>
2018-12-25 15:48:21 +0800

    add UIHelper

---
+++ app/src/main/java/me/liaoheng/wallpaper/util/IUIHelper.java
@@ -0,0 +1,14 @@
+package me.liaoheng.wallpaper.util;
+
+import android.content.Context;
+
+import java.io.File;
+
+/**
+ * @author liaoheng
+ * @version 2018-12-24 11:11
+ */
+public interface IUIHelper {
+    void setWallpaper(Context context, @Constants.setWallpaperMode int mode, File wallpaper)
+            throws Exception;
+}
---
+++ app/src/main/java/me/liaoheng/wallpaper/util/UIHelper.java
@@ -0,0 +1,34 @@
+package me.liaoheng.wallpaper.util;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.io.File;
+
+/**
+ * @author liaoheng
+ * @version 2018-12-24 11:13
+ */
+public class UIHelper implements IUIHelper {
+
+    @Override
+    public void setWallpaper(Context context, int mode, File wallpaper) throws Exception {
+        if (ROM.getROM().isMiui()) {
+            MiuiHelper.setWallpaper(context, mode, wallpaper);
+        } else if (ROM.getROM().isEmui()) {
+            EmuiHelper.setWallpaper(context, mode, wallpaper);
+        } else {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                BingWallpaperUtils.setWallpaper(context, wallpaper);
+            } else {
+                if (mode == Constants.EXTRA_SET_WALLPAPER_MODE_HOME) {
+                    BingWallpaperUtils.setHomeScreenWallpaper(context, wallpaper);
+                } else if (mode == Constants.EXTRA_SET_WALLPAPER_MODE_LOCK) {
+                    BingWallpaperUtils.setLockScreenWallpaper(context, wallpaper);
+                } else {
+                    BingWallpaperUtils.setBothWallpaper(context, wallpaper);
+                }
+            }
+        }
+    }
+}
